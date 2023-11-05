a79ea65349345c545b3b386240766d64976e1c9e
leticia ferreira <leticiaferreira150998@gmail.com>
2023-11-05 17:25:15 -0300

    fofinhos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './views/home/home.component';
+import { HomeComponent } from './shared/home/home.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { UserCardComponent } from './views/hom/user-card/user-card.component';
+import { UserCardComponent } from './shared/user-card/user-card.component';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 @NgModule({
@@ -30,7 +31,7 @@ import { UserCardComponent } from './views/hom/user-card/user-card.component';
     MatToolbarModule, 
     MatIconModule, 
     MatButtonModule,
-    
+    MatDividerModule
      
     ],
 
--- src/app/shared/model/account.model.ts
+++
@@ -1,9 +0,0 @@
-export class Account {  
-        cnpj: any;
-        nameOperador: any;
-        numeroConta: any;
-        agencia: any;
-        nomeEmpresa: any;
-        razaoSocial: any;
-        email: any;  
-}
